af3d44455756f1f2af3513e03a13c3b354178e24
peccu <peccul+github@gmail.com>
2024-06-28 04:46:33 +0000

    show categories and enclosure and merge description and content

--- components/FeedItem.tsx
+++ components/FeedItem.tsx
@@ -8,12 +8,33 @@ interface FeedItemProps {
   isReversed: boolean;
 }
 
+const pickDescription = (
+  description: string | null,
+  content: string | null,
+): string => {
+  if (content == null && description == null) {
+    return "";
+  }
+  if (content == null && description != null) {
+    return description;
+  }
+  if (content != null && description == null) {
+    return content;
+  }
+  if (content != null && description != null) {
+    const lastTrimmed =
+      description.length > 3 ? description.slice(0, -3) : description;
+    return content.startsWith(lastTrimmed) ? content : description;
+  }
+  return "";
+};
+
 const FeedItem: React.FC<FeedItemProps> = ({ item, isReversed }) => {
   return (
-    <Card className="w-screen flex-shrink-0 snap-center flex flex-col pt-4">
+    <Card className="w-screen flex-shrink-0 snap-center flex flex-col pt-4 whitespace-normal">
       <div className="flex-grow overflow-y-auto">
         <CardHeader>
-          <CardTitle className="text-lg whitespace-normal">
+          <CardTitle className="text-lg">
             <a
               href={item.link}
               target="_blank"
@@ -24,24 +45,41 @@ const FeedItem: React.FC<FeedItemProps> = ({ item, isReversed }) => {
             </a>
           </CardTitle>
         </CardHeader>
-        <CardContent>
+        <CardContent className="">
           <p className="text-sm text-gray-500 mb-2">
             {item.feed?.title || "Unknown Feed"}
           </p>
           <p className="text-sm text-gray-500 mb-2">
             {new Date(item.pubDate).toLocaleString()}
           </p>
+          {/* show categories */}
+          <p className="text-sm text-gray-500 mb-2">
+            {item.categories?.map((category) => (
+              <span
+                key={category}
+                className="inline-block bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded mb-2"
+              >
+                {category}
+              </span>
+            ))}
+          </p>
+          {/* show enclosure */}
+          {item.enclosure?.link && (
+            <div className="w-screen -mx-3 md:-mx-6 mb-2 md:mb-4">
+              <img src={item.enclosure?.link} alt="enclosure" />
+            </div>
+          )}
+          {/* show the description and content */}
           <p
-            className="mb-2 whitespace-normal text-justify text-sm"
+            className="mb-2 text-justify text-sm"
             dangerouslySetInnerHTML={sanitize(
-              item.description,
+              pickDescription(item.description, item.content),
               // .replace(/<[^>]*>?/gm, '')
             )}
           ></p>
+          {/* read more */}
           <div className="relative z-20">
-            <div
-              className="mt-4 flex gap-2 justify-center"
-            >
+            <div className="mt-4 flex gap-2 justify-center">
               <a
                 href={item.link}
                 target="_blank"
@@ -53,7 +91,7 @@ const FeedItem: React.FC<FeedItemProps> = ({ item, isReversed }) => {
             </div>
           </div>
         </CardContent>
-        <pre className="text-xs">{item.description}</pre>
+        {/* debug output */}
         <pre className="text-xs">{JSON.stringify(item, null, 2)}</pre>
       </div>
     </Card>
