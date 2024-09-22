6d153786df14ff42a530b949ee5ecbd74080578f
peccu <peccul+github@gmail.com>
2024-09-22 09:52:23 +0000

    add author

--- components/FeedItem.tsx
+++ components/FeedItem.tsx
@@ -89,6 +89,9 @@ const FeedItem: React.FC<FeedItemProps> = ({
         </CardHeader>
         <CardContent className="">
           <p className="text-sm text-gray-500 mb-2">
+            {item.feed?.author || "Unknown Author"}
+          </p> from
+          <p className="text-sm text-gray-500 mb-2">
             {item.feed?.title || "Unknown Feed"}
           </p>
           <p className="text-sm text-gray-500 mb-2">
